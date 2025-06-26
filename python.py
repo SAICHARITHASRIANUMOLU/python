9f3a4635197d9ed20abcf3cde3749c5d6d36f640
SAICHARITHASRIANUMOLU <acharitha2625@gmail.com>
2025-06-26 10:42:54 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,65 @@
+# print("hello world")
+# '''print n fibonocci series'''
+# n=int(input())
+# l=[0,1]
+# a=0
+# b=1
+# for i in range (n-2):
+#     c=a+b
+#     l.append(c)
+#     a=b
+#     b=c
+# print(*l)
+
+# '''you are given a string and you have to count the vowels in the given string both upper and lowe cases and return the total count'''
+# s=input()
+# c=0
+# for each in s:
+#  if each in "aeiouAEIOU":
+#    c+=1
+# print(c) 
+  
+# '''your given a string s your task is to check the string is palindrome or not'''
+# s=input().split()
+# l="".join(s)
+# v=l.lower()
+# if v==v[::-1]:
+#     print(True) 
+# else:
+#     print(False)
+
+# '''check is there are same string or not given two strings a and b'''
+# a=input("enter the string")
+# b=input("enter the string")
+# if a==b:
+#     print(true)
+# else:
+#     print(false)
+
+# '''we have to count the no.of consonents present in a given string(consonants are alphabets except vowels(a,e,i,o,u))'''
+# s=input()
+# x=s.lower()
+# c=0
+# for i in x:
+#     if i in "bcdfghkmnpqrstvwxyz":
+#       c+=1
+# print(c) 
+
+# '''your task is to remove duplicate characters from the string while preserving the prder of the first occurence and return the modifying string'''
+# s=input()
+# v=""
+# for i in s:
+#     if i not in v:
+#         v+=i
+# print(v)
+
+'''is they are eligible for vote???''' 
+if (): 
+    print() 
+else: 
+    print() 
+age = int(input()) 
+if (age >= 18): 
+    print(“You are Eligible”) 
+else: 
+print(“sorry you are not eligible”)
